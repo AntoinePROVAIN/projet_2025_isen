2585338eb1463c2216333ea4a415cbf32283be4c
antoinePROVAIN <antoineprovain@gmail.com>
2025-05-19 10:19:25 +0200

    debut inscr entreprise

--- salesTraction/src/pages/InscriptionEntreprise.tsx
+++ salesTraction/src/pages/InscriptionEntreprise.tsx
@@ -1,10 +1,64 @@
-
+import { useState } from "react";
+import Header from "../components/Header"
+import { useNavigate } from "react-router-dom";
+import "../assets/css/inscriptionEntreprise.css"
 
 function InscriptionEntreprise() {
 
+  const nav = useNavigate();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [siret, setSiret] = useState("");
+    const [nameCompany, setNameCompany] = useState("");
+    const [desc, setDesc] = useState("");
+    // const [error, setError] = useState("");
+
+    const handleClick = () => {
+        nav("/connexion/entreprise");
+    }
+
+    const handleRetour= () => {
+      nav("/");
+    }
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        // setError("");
+    
+        // try {
+        //   const user = await signUpWithEmail(email, password, name);
+        //   console.log("Connecté :", user.email, "uid :", user.uid );
+        // } catch (err: any) {
+        //   setError(err.message);
+        // }
+    };
+
   return (
     <>
-      <h1>Inscription Entreprise</h1>
+      <Header/>
+      <div className='authComponent_wrapper flex flex-col items-center justify-center min-h-screen p-4 md:p-8 pt-20 mt-8'>
+        <div className="authComponent_container w-full bg-white shadow-md rounded-lg p-6 mt-8">
+            <div className='authComponent_content'>
+                  <form onSubmit={handleSubmit}>
+                    <label className="labels">Email</label>
+                    <input type='email' id="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)} className='auth_input w-full mb-3 p-2 border rounded'/>
+                    <label className="labels">SIRET's number</label>
+                    <input type='text' id="siret" name='siret' value={siret} onChange={(e) => setSiret(e.target.value)} className='auth_input w-full mb-3 p-2 border rounded'/>
+                    <label className="labels">Company's name</label>
+                    <input type='text' id="name" name='name' value={nameCompany} onChange={(e) => setNameCompany(e.target.value)} className='auth_input w-full mb-3 p-2 border rounded'/>
+                    <label className="labels">Description</label>
+                    <input type='text' id="desc" name='desc' value={desc} onChange={(e) => setDesc(e.target.value)} className='auth_input w-full mb-3 p-2 border rounded'/>
+                    <label className="labels">Password</label>
+                    <input type='password' id='pwd' name='pwd' value={password} onChange={(e) => setPassword(e.target.value)} className='auth_input w-full mb-3 p-2 border rounded'/>
+                    <div className='auth_buttons flex flex-col md:flex-row justify-between gap-4'>
+                        <input type='submit' value="Inscription" className='auth_button w-full md:w-1/2 py-2 bg-blue-500 text-white rounded' onClick={handleClick}/>
+                        <input type='button' value="Back" className='auth_button w-full md:w-1/2 py-2 bg-gray-500 text-white rounded' onClick={handleRetour}/>
+                    </div>
+                  </form>
+              </div>
+          </div>
+      </div>
     </>
   )
 }
