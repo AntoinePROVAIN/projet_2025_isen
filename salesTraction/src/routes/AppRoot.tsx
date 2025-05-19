04026d61a20abcb6b48e1c0c3583a08f730627b4
antoinePROVAIN <antoineprovain@gmail.com>
2025-05-19 14:39:04 +0200

    nouvelles routes

--- salesTraction/src/routes/AppRoot.tsx
+++ salesTraction/src/routes/AppRoot.tsx
@@ -6,8 +6,9 @@ import ConnexionEtudiant from '../pages/ConnexionEtudiant';
 import InscriptionEntreprise from '../pages/InscriptionEntreprise';
 import InscriptionEtudiant from '../pages/InscriptionEtudiant';
 import ErrorPage from '../pages/ErrorPage';
-import MarketPlaceEntreprise from '../pages/MarketPlaceEntreprise';
-import MarketPlaceEtudiant from '../pages/MarketPlaceEtudiant';
+import ConnexionAdmin from '../pages/admin_connexion';
+import AdminDashboard from '../pages/admin_dashboard';
+import StudentSwipeOffers from '../pages/marketplace_student';
 
 function AppRoot() {
 
@@ -20,8 +21,6 @@ function AppRoot() {
             <Route path="/inscription/entreprise" element={<> <InscriptionEntreprise /> </>} />
             <Route path="/connexion/etudiant" element={<> <ConnexionEtudiant /> </>} />
             <Route path="/connexion/entreprise" element={<> <ConnexionEntreprise /> </>} />
-            <Route path="/marketplace/etudiant" element={<> <MarketPlaceEtudiant /> </>} />
-            <Route path="/marketplace/entreprise" element={<> <MarketPlaceEntreprise /> </>} />
             <Route path="/admin" element={<> <ConnexionAdmin /> </>} />
             <Route path="/admin/dashboard" element={<> <AdminDashboard /> </>} />
             <Route path="/student/dashboard" element={<> <StudentSwipeOffers studentId={1} /> </>} />
