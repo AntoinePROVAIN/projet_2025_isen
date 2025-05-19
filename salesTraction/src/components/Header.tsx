d047e67745224e6730aac539b38eccceefca4709
Dingunus <hugo.chaperon@isen-ouest.yncrea.fr>
2025-05-19 16:01:48 +0200

    trad en place

--- salesTraction/src/components/Header.tsx
+++ salesTraction/src/components/Header.tsx
@@ -3,33 +3,22 @@ import { Menu } from "lucide-react";
 import { X } from "lucide-react";
 import "../assets/css/Header.css";
 import { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import Button from "./Bouton";
 
 
-export default function Header() {
+const Header: React.FC = () => {
     
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const [isMobile, setIsMobile] = useState(false);
-
-    useEffect(() => {
-        const checkIfMobile = () => {
-            setIsMobile(window.innerWidth < 768);
-        };
-
-        checkIfMobile();
-        window.addEventListener('resize', checkIfMobile);
-        return () => {
-            window.removeEventListener('resize', checkIfMobile);
-        };
-    }, []);
-
+    const { t, i18n } = useTranslation();
+    
     const toggleMenu = () => {
         setIsMenuOpen(!isMenuOpen);
     };
 
     return (
         <header className="relative">
-            <div className="container mx-auto px-4 py-3 flex flex-row ">
+            <div className="mx-auto px-4 py-3 flex flex-row ">
                 <button
                     onClick={toggleMenu}
                     className="mr-4 z-50 relative" 
@@ -37,7 +26,7 @@ export default function Header() {
                     {!isMenuOpen ? <Menu size={24} /> : <X size={24}/>}
                     
                 </button>
-                <h1 className="text-4xl">SalesTraction</h1>
+                <h1 className="text-4xl">{t('SalesTraction')}</h1>
                 <div className="w-8"></div>
             </div>
             {/* {isMenuOpen && ( */}
@@ -47,13 +36,13 @@ export default function Header() {
                         <div className="px-4">
                             <ul className="space-y-10">
                             <li>
-                                <a href="#" className="block px-2 py-1">Qui sommes nous ?</a>
+                                <a href="#" className="block px-2 py-1">{t('whoami')}</a>
                             </li>
                             <li>
-                                <a href="#" className="block px-2 py-1">Avis</a>
+                                <a href="#" className="block px-2 py-1">{t('opinion')}</a>
                             </li>
                             <li>
-                                <a href="#" className="block px-2 py-1">FAQ</a>
+                                <a href="#" className="block px-2 py-1">{t('faq')}</a>
                             </li>
                             </ul>
                         </div>
@@ -61,8 +50,8 @@ export default function Header() {
                     <span className="my-10"></span>
                     <span className="my-10"></span>
                     <div className="space-y-10 py-10 px-16 flex flex-col justify-center">
-                        <Button onClick={()=>console.log('Etudiant')} children='Je suis un étudiant' />
-                        <Button onClick={()=>console.log('Startup')} children='Je suis une startup' />
+                        <Button onClick={()=>console.log('Etudiant')} children={t('student')} />
+                        <Button onClick={()=>console.log('Startup')} children={t('startup')} />
                     
                     </div>
                 </div>
@@ -71,3 +60,5 @@ export default function Header() {
         </header>
     );
 };
+
+export default Header;
--- salesTraction/src/pages/LandingPage.tsx
+++ salesTraction/src/pages/LandingPage.tsx
@@ -1,18 +1,27 @@
 
+import { useNavigate } from 'react-router-dom'
 import Button from '../components/Bouton'
 import Header from '../components/Header'
 
 function LandingPage() {
 
+    const nav = useNavigate();
+
   return (
     <>
       <Header />
       <div className='container'>
-        <h1>Rencontre ton commercial</h1>
-        <img src="" alt="" />
-        <Button onClick={()=>console.log('Etudiant')} children='Je suis un étudiant' />
-        <Button onClick={()=>console.log('Startup')} children='Je suis une startup' />
-    
+        
+        <h1 className='my-10 text-3xl font-bold text-center'>Rencontre ton commercial</h1>
+        <div className='bg-black w-full h-80'>
+            <img src="" alt="image de présentation" />
+        </div>
+
+        <span className='my-10'></span>
+        <div className="space-y-10 py-10 px-16 flex flex-col justify-center">
+            <Button onClick={()=>nav("/connection?personne=student")} children='Je suis un étudiant' />
+            <Button onClick={()=>nav("/connection?personne=student")} children='Je suis une startup' />
+        </div>
       </div>
       </>
   )
